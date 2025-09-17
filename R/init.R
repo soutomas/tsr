f4013e60b145bf0adf62e578c6b23f30a67d6022
Sou <souto1@novartis.net>
2025-09-17 15:23:41 +0200

    Added `summ_cont`

--- R/init.R
+++ R/init.R
@@ -1,44 +1,65 @@
-#===================================================================================
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Filename : init.R
 # Use      : Convenient functions
 # Author   : Tomas Sou
 # Created  : 2025-08-29
-# Updated  : 2025-09-12
-#===================================================================================
+# Updated  : 2025-09-17
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Notes
 # na
-#===================================================================================
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Updates
 # na
-#===================================================================================
-#' Generate source label
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Functions
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#' Copy to destination
 #'
-#' Generate a source label with file path and run time.
-#' In interactive sessions, this function uses `rstudioapi` to get the file path.
-#' It is designed to work in a script file in RStudio when running interactively.
-#' It will return empty when running in the console.
+#' Copy file to destination and rename if desired.
 #'
-#' @param span <num> Number of lines: either 1 or 2
-#' @param omit [optional] <chr> Text to omit from the label
-#' @returns A label with source file path and run time
+#' @param fpath File path of the source file
+#' @param tag [optional] <chr> Tag to the filename
+#' @param des Destination folder
+#' @export
+fc = function(fpath,tag="-",des="/home/souto1/Documents/"){
+  # Copy
+  fname = basename(fpath)
+  fstem = tools::file_path_sans_ext(fname)
+  fext = tools::file_ext(fname)
+  file.copy(fpath,des,overwrite=T)
+  # Rename
+  today = format(Sys.time(),"%y%m%d")
+  fname_to = paste0(fstem,tag,today,".",fext)
+  fpath1 = file.path(des,fname)
+  fpath2 = file.path(des,fname_to)
+  file.rename(fpath1,fpath2)
+}
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# library(flextable)
+
+#' flextable wrapper
+#'
+#' Sugar function for default flextable output.
+#'
+#' @param d A data frame
+#' @param fnote [optional] <chr> Footnote
+#' @param ttl [optional] <chr> Title
+#' @returns A flextable object
 #' @export
 #' @examples
-#' label_src(1)
-label_src = function(span=2,omit=""){
-  loc_src = getwd()
-  if(interactive()) loc_src = rstudioapi::getActiveDocumentContext()$path |> dirname()
-  fname_src = knitr::current_input() |> gsub(".rmarkdown",".qmd",x=_)
-  if(interactive()) fname_src = rstudioapi::getActiveDocumentContext()$path |> basename()
-  fpath_src  = file.path(loc_src,fname_src)
-  tz = paste0("Run: ",format(Sys.time(),usetz=T))
-  lab1 = paste0("Source:",fpath_src,"\n",tz) |> gsub(omit,"",x=_)
-  lab2 = paste0("Source:",loc_src,"/\n",fname_src,"\n",tz) |> gsub(omit,"",x=_)
-  out = lab1
-  if(span==2) out = lab2
+#' mtcars |> ft()
+ft = function(d,fnote=NULL,ttl=NULL){
+  out = d %>%
+    flextable::flextable() |>
+    flextable::autofit() |>
+    flextable::colformat_double(digits=3) |>
+    flextable::add_footer_lines(fnote) |>
+    flextable::add_header_lines(ttl)
   return(out)
 }
 
-#===================================================================================
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # library(patchwork)
 
 #' Add source label to ggplot
@@ -67,54 +88,26 @@ ggsrc = function(plt,span=2,size=8,col="grey55",lab=NULL,omit=""){
   return(out)
 }
 
-#===================================================================================
-#' Copy to destination
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#' Set colour scales
 #'
-#' Copy file to destination and rename if desired.
+#' Set colour scales for the desired number of colours.
 #'
-#' @param fpath File path of the source file
-#' @param tag [optional] <chr> Tag to the filename
-#' @param des Destination folder
-#' @export
-fc = function(fpath,tag="-",des="/home/souto1/Documents/"){
-  # Copy
-  fname = basename(fpath)
-  fstem = tools::file_path_sans_ext(fname)
-  fext = tools::file_ext(fname)
-  file.copy(fpath,des,overwrite=T)
-  # Rename
-  today = format(Sys.time(),"%y%m%d")
-  fname_to = paste0(fstem,tag,today,".",fext)
-  fpath1 = file.path(des,fname)
-  fpath2 = file.path(des,fname_to)
-  file.rename(fpath1,fpath2)
-}
-
-#===================================================================================
-# library(flextable)
-
-#' flextable wrapper
-#'
-#' Sugar function for default flextable output.
-#'
-#' @param d A data frame
-#' @param fnote [optional] <chr> Footnote
-#' @param ttl [optional] <chr> Title
-#' @returns A flextable object
+#' @param n <num> Number of colours to output
+#' @param show <lgl> `TRUE` to show the output colours
+#' @returns Hex code of colours that can be used for plotting
 #' @export
 #' @examples
-#' mtcars |> ft()
-ft = function(d,fnote=NULL,ttl=NULL){
-  out = d %>%
-    flextable::flextable() |>
-    flextable::autofit() |>
-    flextable::colformat_double(digits=3) |>
-    flextable::add_footer_lines(fnote) |>
-    flextable::add_header_lines(ttl)
+#' hcln(6,FALSE)
+#' hcln(8,TRUE)
+hcln = function(n,show=FALSE){
+  hues = seq(15, 375, length=n+1)
+  out = hcl(h=hues, c=100, l=65)[1:n]
+  if(show) scales::show_col(out)
   return(out)
 }
 
-#===================================================================================
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #' kable wrapper
 #'
 #' Sugar function for default kable output.
@@ -133,21 +126,67 @@ kbl2 = function(d,fnote=NULL,cap=NULL){
     kableExtra::footnote(fnote,general_title="")
 }
 
-#===================================================================================
-#' Set colour scales
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#' Generate source label
 #'
-#' Set colour scales for the desired number of colours.
+#' Generate a source label with file path and run time.
+#' In interactive sessions, this function uses `rstudioapi` to get the file path.
+#' It is designed to work in a script file in RStudio when running interactively.
+#' It will return empty when running in the console.
 #'
-#' @param n <num> Number of colours to output
-#' @param show <lgl> `TRUE` to show the output colours
-#' @returns Hex code of colours that can be used for plotting
+#' @param span <num> Number of lines: either 1 or 2
+#' @param omit [optional] <chr> Text to omit from the label
+#' @returns A label with source file path and run time
 #' @export
 #' @examples
-#' hcln(6,FALSE)
-#' hcln(8,TRUE)
-hcln = function(n,show=FALSE){
-  hues = seq(15, 375, length=n+1)
-  out = hcl(h=hues, c=100, l=65)[1:n]
-  if(show) scales::show_col(out)
+#' label_src(1)
+label_src = function(span=2,omit=""){
+  loc_src = getwd()
+  if(interactive()) loc_src = rstudioapi::getActiveDocumentContext()$path |> dirname()
+  fname_src = knitr::current_input() |> gsub(".rmarkdown",".qmd",x=_)
+  if(interactive()) fname_src = rstudioapi::getActiveDocumentContext()$path |> basename()
+  fpath_src  = file.path(loc_src,fname_src)
+  tz = paste0("Run: ",format(Sys.time(),usetz=T))
+  lab1 = paste0("Source:",fpath_src,"\n",tz) |> gsub(omit,"",x=_)
+  lab2 = paste0("Source:",loc_src,"/\n",fname_src,"\n",tz) |> gsub(omit,"",x=_)
+  out = lab1
+  if(span==2) out = lab2
+  return(out)
+}
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#' Summary for continuous variables
+#'
+#' Summarise selected continuous variables.
+#' Variables can be grouped beforehand.
+#'
+#' @param d A data frame
+#' @param cols Columns to summarise in a vector `c()` as unquote names or character
+#' @param xname [optional] <chr> Character to omit in output column names
+#' @returns A data frame of summarised variables
+#' @export
+#' @examples
+#' mtcars |> summ_cont(mpg)
+#' mtcars |> summ_cont(cols=mpg,xname="mpg_")
+#' mtcars |> group_by(vs,am) |> summ_cont(mpg,"mpg_")
+#' mtcars |> group_by(vs,am) |> summ_cont(cols=c(mpg,disp))
+summ_cont = function(d,cols,xname=""){
+  out = d |>
+    dplyr::select({{cols}}) |>
+    dplyr::summarise(
+      dplyr::across(
+        dplyr::where(dplyr::is.numeric),
+        list(
+          Mean = ~mean(.x, na.rm=T),
+          Med = ~median(.x, na.rm=T),
+          SD  = ~sd(.x, na.rm=T),
+          Min = ~min(.x, na.rm=T),
+          P10 = ~quantile(.x, 0.1, na.rm=T),
+          P90 = ~quantile(.x, 0.9, na.rm=T),
+          Max = ~max(.x, na.rm=T)
+        )
+      )
+    ) |>
+    dplyr::rename_with(~gsub(xname,"",.x))
   return(out)
 }
